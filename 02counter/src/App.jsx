fdf13edaeed9221798de91b0fd124d47ff520987
ChinuD <dhokeychinmay.com>
2024-12-15 10:08:11 +0530

    fourth commit

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -12,6 +12,9 @@ function App() {
   const addValue = () =>{
     if(counter<20){
       setCounter(counter+1)
+      setCounter(counter+1)
+      setCounter(counter+1)
+      setCounter(counter+1)
     }
   } 
   const subtractValue = () =>{
