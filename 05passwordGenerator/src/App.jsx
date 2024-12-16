1027f88d9bb5c5d99881f9ca426b6253634ee674
ChinuD <dhokeychinmay.com>
2024-12-16 11:15:34 +0530

    fifth commit

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState, useCallback } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numbersAllowed, setNumbersAllowed] = useState(false);
+  const [charactersAllowed, setCharactersAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+
+    if (numbersAllowed) str += "1234567890";
+    if (charactersAllowed) str += "!@#$%^&*()[]{}~'";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, setCharactersAllowed, setNumbersAllowed, setPassword]);
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray">
+        <h1 className="text-white text-center my-4">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+          />
+
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+            copy
+          </button>
+        </div>
+
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input type="range" 
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer "
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
